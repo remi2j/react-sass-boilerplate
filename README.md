62fad110d05ffcbbdb25b85cae188e78f1a885b9
Rémi de Juvigny <remidejuvigny@gmail.com>
2018-05-14 18:41:28 +0200

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# React + Sass + Webpack Boilerplate
+
+Kickstart your projects with an advanced config :boom:
+
+## Features
+
+* :recycle: Compile ES6+ and JSX into ES5
+* :lipstick: Compile Sass into regular and normalized CSS
+* :zap: Hot reloading
+* :lock: Error overlay screen (same one as [create-react-app](https://github.com/facebook/create-react-app))
+
+## Installation
+
+* Run `git clone https://github.com/remi2j/react-sass-boilerplate.git`
+* Tweak `webpack.config.js` and have fun
+
+## Upcoming features
+
+* :heavy_plus_sign: SCSS support
+* :factory: Create builds optimized for production
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const ErrorOverlayPlugin = require("error-overlay-webpack-plugin")
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new ErrorOverlayPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+}
